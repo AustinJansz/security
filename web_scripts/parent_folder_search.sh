96ee1dcd04be4b95dcf80c5eb7bc406a7972bcb8
TheUhrMacher <theuhrmacher@protonmail.com>
2020-03-02 12:51:51 -0500

    Update to more general function (check to see if path can be visited)

--- web_scripts/parent_folder_search.sh
+++ web_scripts/parent_folder_search.sh
@@ -1,6 +1,6 @@
 # Simple help message for use when learning how to use the tool
 if [ "$1" == "-h" ]; then
-  echo "./parent_folder_search.sh [DOMAIN_NAME] [PARAMETER] [KNOWN_FILE_PATH]"
+  echo "./parent_folder_search.sh [DOMAIN_NAME] [PARAMETER] [KNOWN_FILE_PATH] ["
   exit 0
 fi
 
@@ -9,6 +9,10 @@ parent_folder=""
 # Iterate through 10 parent directories
 for i in {0..10}
 do
-	echo "$1$2=$parent_folder$3"
+	# Create variable for current file path check
+	current_path="$1$2=$parent_folder$3"
+	# Curl the folder structure to check the existece of the file with the flag
+	curl -s "$current_path" | head -1 | cut -d ' ' -f 2 | grep -qiv '200' || echo "Found: $current_path"
+	# Move up to the next parent
 	parent_folder+="../"
 done

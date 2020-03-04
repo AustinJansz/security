83ef8fd21e4cfd8023858938fe59bd3b56c377ee
TheUhrMacher <theuhrmacher@protonmail.com>
2020-03-04 11:28:27 -0500

    Fixed version finders to allow user to enter version number with dots (111 --> 1.1.1)

--- web_scripts/wordpress_version-meta.sh
+++ web_scripts/wordpress_version-meta.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Simple help message for use when learning how to use the tool
 if [ "$1" == "-h" ]; then
-  echo "./wordpress_version-meta.sh DOMAIN_NAME WORDPRESS_VERSION(DOTS REMOVED)"
+  echo "./wordpress_version-meta.sh DOMAIN_NAME WORDPRESS_VERSION"
   exit 0
 fi
 
 # Pull the main page attached to the passed domain and extract the version
-wp_version="$(curl -s "$1" | egrep -oi "Wordpress \d\.\d\.\d" | cut -d '=' -f 2)"
+wp_version="$(curl -s "$1" | egrep -oi "Wordpress \d\.\d\.\d" | sort -u | cut -d ' ' -f 2)"
 # Compares the found version to the reference version
-if [  "${wp_version//./}" -le "$2" ]; 
+if [  "${wp_version//./}" -le "${2//./}" ]; 
 	then echo "Wordpress $wp_version found - VULNERABLE"; 
 	else echo "Wordpress $wp_version found - SECURE"; 
 fi
